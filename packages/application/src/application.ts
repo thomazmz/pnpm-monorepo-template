c10f9b9932e05347147d2ced9faa396a50de9a35
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2024-05-14 00:23:03 -0700

    include application example

---
+++ packages/application/src/application.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+import { concatName }  from '@monorepo/library'
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send(`Hello ${concatName('thomaz', 'zandonotto')}!`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
