28b817b74570bd3c2b7ebf21fe32fc1c6b4b1031
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2025-08-13 01:16:15 -0700

    Place some working code into app.ts file

--- packages/application/src/app.ts
+++ packages/application/src/app.ts
@@ -1,19 +1,3 @@
-// import * as Application from '@monorepo/application'
-// import * as Library from '@monorepo/library'
-// const child_1 = new Library.Child()
-// console.log(child_1)
-
-// const parent_1 = new Library.Parent()
-// console.log(parent_1)
-
-// import { Child } from '@monorepo/library/child'
-// const child_2 = new Child()
-// console.log(child_2)
-
-// import { Parent } from '@monorepo/library/parent'
-// const parent_2 = new Parent()
-// console.log(parent_2)
-
-
-
-console.log('hey!')
+import * as Application from '@monorepo/application/services'
+const service = new Application.Service()
+console.log(service.name)
